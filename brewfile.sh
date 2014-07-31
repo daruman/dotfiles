1253387aa4859ee2a352e68b9d17171ef3b51678
daruman <daruman.atl@gmail.com>
2014-07-31 11:53:02 +0900

    create brewfile.sh

---
+++ brewfile.sh
@@ -0,0 +1,178 @@
+# Homebrewを最新にする
+# =============================================================================
+update || true
+
+
+# Repositoryを追加する
+# =============================================================================
+
+# homebrew-cask用Repository
+tap caskroom/homebrew-cask || true
+
+# homebrew-cask用Repository(version指定可能、ベータ版や日本語版が欲しい際に)
+tap caskroom/homebrew-versions || true
+
+# バイナリインストール対応Repository
+tap homebrew/binary || true
+
+# バージョン指定で古いものに固定する場合に使用
+tap homebrew/versions || true
+# tap caskroom/versions || true
+
+# php用(dupesは依存用)
+tap homebrew/dupes || true
+tap josegonzalez/homebrew-php || true
+
+# Install Packages
+# =============================================================================
+
+# Packages for brew-cask
+install brew-cask || true
+
+# Shell
+install zsh --disable-etcdir || true
+
+## Languages/xxenv
+install rbenv || true
+install ruby-build || true
+install rbenv-default-gems || true
+install pyenv || true
+install pyenv-virtualenv
+install python3 || true
+install plenv || true
+install perl-build || true
+# --HEADつけないと怒られる
+install phpenv --HEAD || true
+# phpenvのpluginとして下記コマンドでInstallしたほういい？
+# $ git clone git@github.com:CHH/php-build.git ~/.phpenv/plugins/php-build
+# install php-build || true
+install josegonzalez/php/composer
+install php53-xdebug || true
+install php53-apc || true
+install go --cross-compile-common || true
+install lua
+install luajit
+
+# Middleware
+install httpd || true
+install phantomjs || true
+install git || true
+install mysql || true
+install memcached || true
+install sqlite || true
+#install nginx ||  true
+
+# DevEnv
+#install packer      || true
+#install dnsmasq     || true
+#install ansible     || true
+
+# Node.js
+install nodebrew || true
+# install node || true
+# install nvm || true
+
+# tools
+install macvim --with-lua --with-luajit --HEAD || true
+install tmux || true
+
+# Commands\Utils
+install curl || true
+install tree || true
+install openssl || true
+install jq || true
+install wget || true
+install ctags || true
+install ag || true
+install tig || true
+install autoconf || true
+install automake || true
+install grc || true
+install source-highlight || true
+install rmtrash || true
+
+# Dependent
+#   php
+install jpeg || true
+install re2c || true
+install libmcrypt || true
+install libpng || true
+
+# 必要かよくわからない
+install markdown || true
+install nkf || true
+install pcre || true
+install gcc || true
+install mcrypt || true
+install imagemagick || true
+install cmake || true
+# install libtool || true
+
+# 検討中
+# install jenkins || true
+# install hub || true
+
+
+# Install .dmg
+# =============================================================================
+
+# JAVA
+#   JDK8
+cask install java
+#   JDK7
+# cask install java7
+
+# Application
+cask install iterm2
+cask install sequel-pro
+cask install sourcetree
+cask install vlc
+cask install cyberduck
+cask install dash
+cask install alfred
+cask install coteditor
+#   alfredでhomebrew経由でインストールしたアプリも検索出来るようにする
+cask alfred link
+
+# Browser
+cask install google-chrome
+cask install google-chrome-canary
+cask install firefox-ja
+cask install opera
+
+# DevEnv
+# cask install virtualbox
+# cask install vagrant
+
+# Utility
+cask install google-japanese-ime
+cask install caffeine
+cask install karabiner
+cask install appcleaner
+cask install the-unarchiver
+
+# 検討中
+# cask install slate
+# cask install dropbox
+# cask install hipchat
+# cask install xtrafinder
+# cask install skitch
+# cask install lastpass-universal
+# cask install keepassx
+
+# 既にインストールされているFormulaを最新にする
+# =============================================================================
+upgrade || true
+
+# .dmg update
+# =============================================================================
+cask update
+
+# ~/Applications/にシンボリックリンク作成
+# =============================================================================
+linkapps
+
+# 不要ファイルの削除
+# =============================================================================
+cleanup
+cask cleanup
