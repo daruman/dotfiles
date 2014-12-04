02e70b3c9e90b1a372dba79101572bd558fbacdb
daruman <daruman.atl@gmail.com>
2014-12-04 21:55:02 +0900

    lastpass-cli追加

--- setupShell/brewShell/brewEnv/ms628.sh
+++ setupShell/brewShell/brewEnv/ms628.sh
@@ -1,6 +1,6 @@
 # 社用MBP用
 
-# brew install lastpass-cli --with-pinentry --with-doc
+brew install lastpass-cli --with-pinentry --with-doc
 
 # LastPassはhomebrewではpkgを落とすが実行してないので、インストールする
 brew cask install lastpass
