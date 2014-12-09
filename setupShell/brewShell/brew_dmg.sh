f133a84eb381f8ecf2709ab2e5d096088e5329f8
daruman <daruman.atl@gmail.com>
2014-12-09 16:46:47 +0900

    GoogleIMEのインストール後の設定についてコメント追記

--- setupShell/brewShell/brew_dmg.sh
+++ setupShell/brewShell/brew_dmg.sh
@@ -38,6 +38,9 @@ brew cask install the-unarchiver
 
 # Google日本語入力はhomebrewではpkgを落とすが実行してないので、インストールする
 # password聞かれるので入力が間に合えばインストールしてるかも
+# 起動後は以下設定追加
+# [環境設定] → [一般] → [キー設定] → [キー設定の選択 > 編集]にて以下キー追加
+# 入力文字なし	Escape	キャンセル後 IME を無効化
 brew cask install google-japanese-ime
 sudo installer -pkg /opt/homebrew-cask/Caskroom/google-japanese-ime/latest/GoogleJapaneseInput.pkg -tgt /
 
