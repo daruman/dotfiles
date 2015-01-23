c645633d04ee209b7dd1c9ef44a85c28b59f8342
daruman <daruman.atl@gmail.com>
2015-01-23 15:21:03 +0900

    font取得時に依存パッケージも取得するよう設定追加

--- setup/brewShell/brew_font.sh
+++ setup/brewShell/brew_font.sh
@@ -1,6 +1,11 @@
 THIS_PATH=${BASH_SOURCE:-$0}
 echo "******************* [${THIS_PATH##*/}] start " `date +'%Y/%m/%d %H:%M:%S'` " *******************"
 
+
+# depends
+# brew cask install xquartz
+brew install pkg-config
+
 # Ricty font
 brew install ricty
 cp -f /usr/local/Cellar/ricty/*/*/fonts/Ricty*.ttf ~/Library/Fonts/
